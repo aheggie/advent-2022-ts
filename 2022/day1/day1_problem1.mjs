564617f926035b1bc2614d63bbfbef809bc785ad
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-02 22:20:41 -0500

    sum each elf into a single integer 🧝🏿‍♀️

--- 2022/day1/day1_problem1.mjs
+++ 2022/day1/day1_problem1.mjs
@@ -1,24 +1,26 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync } from "node:fs";
 
 //import the data
-const rawData = readFileSync("./alexandra/input.txt", "utf8")
+const rawData = readFileSync("./alexandra/input.txt", "utf8");
 
 //split into separate lists for each elf
-const elfStrings = rawData.split("\n\n")
+const elfStrings = rawData.split("\n\n");
 
 //sum each elf
-const processEachElf = elfString => {
-    return elfString.split("\n").map(numberString => parseInt(numberString))
-    
-}
+const processEachElf = (elfString) =>
+  elfString
+    // split into list of numberStings
+    .split("\n")
+    // convert from STING to number
+    .map((numberString) => parseInt(numberString))
+    // sum each elf to a single integer
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-const elves = elfStrings.map(processEachElf)
-
+const elves = elfStrings.map(processEachElf);
 
 //find max elf
 
-
 //print out result
 // console.log(rawData, JSON.stringify(rawData))
-const output = elves
-console.log(output)
+const output = elves;
+console.log(output);
