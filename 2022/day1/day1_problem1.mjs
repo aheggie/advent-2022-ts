3d24f61a1d36311de1051e0082adf4bfc99bd2b3
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-02 21:58:48 -0500

    splitting elves into individual elf lists 🧝🏽‍♀️

--- 2022/day1/day1_problem1.mjs
+++ 2022/day1/day1_problem1.mjs
@@ -2,11 +2,23 @@ import { readFileSync } from 'node:fs';
 
 //import the data
 const rawData = readFileSync("./alexandra/input.txt", "utf8")
+
 //split into separate lists for each elf
+const elfStrings = rawData.split("\n\n")
 
 //sum each elf
+const processEachElf = elfString => {
+    return elfString.split("\n")
+    
+}
+
+const elves = elfStrings.map(processEachElf)
+
 
 //find max elf
 
+
 //print out result
-console.log(rawData)
+// console.log(rawData, JSON.stringify(rawData))
+const output = elves
+console.log(output)
