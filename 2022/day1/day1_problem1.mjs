f265cf50955429678cd51361cec25ee1df18382e
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-02 21:22:06 -0500

    loading the data! 🧝‍♀️

---
+++ 2022/day1/day1_problem1.mjs
@@ -0,0 +1,12 @@
+import { readFileSync } from 'node:fs';
+
+//import the data
+const rawData = readFileSync("./alexandra/input.txt", "utf8")
+//split into separate lists for each elf
+
+//sum each elf
+
+//find max elf
+
+//print out result
+console.log(rawData)
