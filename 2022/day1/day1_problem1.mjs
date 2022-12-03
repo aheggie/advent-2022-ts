3a00139959f73f5cc916cacee0b79df62044170a
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-02 23:24:57 -0500

    we solved it!

--- 2022/day1/day1_problem1.mjs
+++ 2022/day1/day1_problem1.mjs
@@ -6,6 +6,8 @@ const rawData = readFileSync("./alexandra/input.txt", "utf8");
 //split into separate lists for each elf
 const elfStrings = rawData.split("\n\n");
 
+const badElf = elfStrings[253];
+
 //sum each elf
 const processEachElf = (elfString) =>
   elfString
@@ -18,13 +20,45 @@ const processEachElf = (elfString) =>
 
 const elfSums = elfStrings.map(processEachElf);
 
-//find max elf
-const elfMax = elfSums.slice(0).sort((sumA, sumB) => sumB - sumA)[0];
+// FIX THIS
+// const badElfList = badElf.trim().split("\n");
+// const badElfNumList = badElfList.map((numberString) => parseInt(numberString));
+// const badElfSum = badElfNumList.reduce((acc, cur) => acc + cur);
 
-const elfLocation = elfSums.indexOf(elfMax);
+// const isThereNaN = elfSums.includes(NaN);
+// const indexOfNaN = elfSums.findIndex((n) => Number.isNaN(n));
+
+//find max elf
+const elvesDescending = elfSums.slice(0).sort((sumA, sumB) => sumB - sumA);
+
+const elfMax = elvesDescending[0];
+
+const elfMaxLocation = elfSums.indexOf(elfMax);
+
+// find sum of top three elves
+const topThreeSum = elvesDescending.slice(0, 3).reduce((acc, cur) => acc + cur);
 
 // find min elf
 
+const elfMin = elfSums.slice(0).sort((sumA, sumB) => sumA - sumB)[0];
+
+const elfMinLocation = elfSums.indexOf(elfMin);
+
 //print out result
-const output = { elfMax, elfLocation };
+const output = {
+  elvesLength: elfStrings.length,
+  badElf,
+  badElfList,
+  badElfNumList,
+  badElfSum,
+  indexOfNaN,
+  elfMax,
+  elfMaxLocation,
+  elfMin,
+  elfMinLocation,
+  topThreeSum,
+  isThereNaN,
+};
 console.log(output);
+
+//emily messing around (sorry Alex)
