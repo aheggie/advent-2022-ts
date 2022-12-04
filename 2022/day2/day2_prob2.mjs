55a6fe0dc3e3f53f23d8d9f698b443e2caeb6420
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-03 20:12:37 -0500

    outcome wrong idk

--- 2022/day2/day2_prob2.mjs
+++ 2022/day2/day2_prob2.mjs
@@ -1,7 +1,7 @@
 import { readFileSync } from "node:fs";
 
-const opponentChoiceObj = { A: "Rock", B: "Paper", C: "Scissors" }; //makeRPSDecoder("A", "B", "C");
-const goalObj = { X: "Lose", Y: "Draw", Z: "Win" };
+const opponentChoiceLookup = { A: "Rock", B: "Paper", C: "Scissors" }; //makeRPSDecoder("A", "B", "C");
+const goalLookup = { X: "Lose", Y: "Draw", Z: "Win" };
 
 // if THEY play rock and I want to X what do I play
 const makeOutcomeObj = (Win, Lose, Draw) => ({ Win, Lose, Draw });
@@ -11,7 +11,10 @@ const oppPaperObj = makeOutcomeObj("Scissors", "Rock", "Paper");
 
 const oppScissorsObj = makeOutcomeObj("Rock", "Paper", "Scissors");
 
-const arrayOfRounds = readFileSync("./data/input.txt", "utf8")
+const choiceScoreLookup = { Rock: 1, Paper: 2, Scissors: 3 };
+const outcomeScoreLookup = { Win: 6, Lose: 0, Draw: 3 };
+
+const finalScore = readFileSync("./data/input.txt", "utf8")
   // clean white space
   .trim()
   // split into rounds
@@ -20,17 +23,30 @@ const arrayOfRounds = readFileSync("./data/input.txt", "utf8")
   .map((roundStr) => roundStr.split(" "))
   // process each array of two strings into an object with each choice
   .map((roundArr) => ({
-    opp: processOpponent(roundArr[0]),
-    goal: processSelf(roundArr[1]),
+    opp: opponentChoiceLookup[roundArr[0]],
+    goal: goalLookup[roundArr[1]],
   }))
   // play out each game and return the score
-  .map(playGame)
+  .map(({ opp, goal }) => {
+    switch (opp) {
+      case "Rock":
+        return { choice: oppRockObj[goal], goal: goal };
+      case "Paper":
+        return { choice: oppScissorsObj[goal], goal: goal };
+      case "Scissors":
+        return { choice: oppScissorsObj[goal], goal: goal };
+    }
+  })
+  // score each game
+  .map(({ choice, goal }) => {
+    return choiceScoreLookup[choice] + outcomeScoreLookup[goal];
+  })
   // sum
   .reduce((acc, cur) => acc + cur);
 
 console.dir(
   {
-    arrayOfRounds,
+    finalScore,
   },
   { showHidden: true, depth: null, maxArrayLength: null }
 );
