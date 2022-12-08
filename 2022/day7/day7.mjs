440df1f8fcdeb9dd92652b29f21dbd5f8424331d
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-12-07 23:03:54 -0500

    checking for lks repeats

--- 2022/day7/day7.mjs
+++ 2022/day7/day7.mjs
@@ -19,10 +19,13 @@ const isNumeric = (str) => {
 };
 
 const parsedData = rawData.map((lineStr, idx) => {
+  let currentDir = "/";
   const lineArr = lineStr.trim().split(" ");
   if (lineArr[0] === "$") {
     if (lineArr[1] === "cd") {
-      return { type: "instruction", action: "cd", to: lineArr[2] };
+      const to = lineArr[2];
+      currentDir = to;
+      return { type: "instruction", action: "cd", to };
     } else if (lineArr[1] === "ls") {
       return { type: "instruction", action: "ls" };
     } else {
@@ -31,16 +34,61 @@ const parsedData = rawData.map((lineStr, idx) => {
       );
     }
   } else if (lineArr[0] === "dir") {
-    return { type: "directory", name: lineArr[1], size: undefined };
+    return {
+      type: "directory",
+      name: lineArr[1],
+      childOf: currentDir,
+      size: 0,
+    };
   } else if (isNumeric(lineStr[0])) {
-    return { type: "file", name: lineArr[1], size: lineArr[0] };
+    return {
+      type: "file",
+      name: lineArr[1],
+      childOf: currentDir,
+      size: lineArr[0],
+    };
   } else {
     throw new Error(`error instruction type at index: ${idx}: " ${lineStr} "`);
   }
 });
 
+//the following code is to establish whether all ls operation happen only on newly seen directories
+//i.e. can an ls operation indicate we are seeing a directory for the first time?
+//it CANT - ls is run up to 16 times on visiting an already-seen directory
+const justChangesWithLists = parsedData
+  .filter((instrObj, idx, arr) => {
+    if (
+      instrObj.type === "instruction" &&
+      instrObj.action === "cd" &&
+      arr[idx + 1].type === "instruction" &&
+      arr[idx + 1].action === "ls"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  })
+  .map(({ to }) => to);
+
+const justChangesWithListsSet = new Set(justChangesWithLists);
+
+const areAllLsOpsOnNewDirs =
+  justChangesWithLists.length === justChangesWithListsSet.size;
+
+const repeats = justChangesWithLists.length - justChangesWithListsSet.size;
+
+const countRepeats = justChangesWithLists.reduce((acc, curr) => {
+  if (acc.hasOwnProperty(curr)) {
+    acc[curr] += 1;
+  } else {
+    acc[curr] = 1;
+  }
+  return acc;
+}, {});
+//end section checking for ls repeats
+
 console.dir(
-  { parsedData },
+  { areAllLsOpsOnNewDirs, repeats, countRepeats },
   {
     showHidden: true,
     depth: null,
